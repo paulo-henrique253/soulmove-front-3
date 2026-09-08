1dbfeca84f36210c7aaf55be4cd83fea476f1c84
Danielle Kagan <rm569359@fiap.com.br>
2026-09-08 17:29:54 -0300

    Feat: componente para layout e navegacao router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { Route, Routes } from "react-router"
+import MainLayout from "./layouts/MainLayout"
+import Index from "./pages/Index"
+import Sobre from "./pages/Sobre"
+import Funcionalidades from "./pages/Funcionalidades"
+import Roadmap from "./pages/Roadmap"
+import Integrantes from "./pages/Integrantes"
+import Faq from "./pages/Faq"
+import Contato from "./pages/Contato"
+
 function App() {
   return (
-    <>
-    </>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route index element={<Index />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/funcionalidades" element={<Funcionalidades />} />
+        <Route path="roadmap" element={<Roadmap />} />
+        <Route path="/integrantes" element={<Integrantes />} />
+        <Route path="/contato" element={<Contato />} />
+        <Route path="/faq" element={<Faq />} />
+      </Route>
+    </Routes>
   )
 }
 
