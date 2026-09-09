dff6669f26ccbb0540de302591e58f3089b3fea8
Felle <juliafb1903@gmail.com>
2026-09-09 19:39:11 -0300

    feat: estilo footer e menu do footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,30 @@ import MenuFooter from "./MenuFooter";
 
 function Footer() {
     return (
-        <footer className="">
-            
+        <footer className="
+        w-full
+        flex
+        flex-col
+        justify-center
+        items-center
+        gap-6
+        p-4
+        bg-linear-to-r
+        from-sky-400
+        to-indigo-500
+
+        md:gap-6
+        md-p8
+        ">
+
             <MenuFooter />
 
-            <p className="">
+            <p className="
+            text-center
+            font-['Lexend_Deca']
+            text-[clamp(.7rem,2vw,.95rem)]
+            text-white
+            ">
                 © 2026 Antípoda. Todos os direitos reservados.
             </p>
         </footer>
--- src/components/MenuFooter.tsx
+++ src/components/MenuFooter.tsx
@@ -1,33 +1,49 @@
 import { Link } from "react-router";
 
 function MenuFooter() {
+    const estilosLink = `
+    text-white
+    font-['Momo_Trust_Display']
+    font-normal
+    text-[clamp(.75rem,2.5vw,1rem)]
+    no-underline
+    whitespace-nowrap
+    hover:opacity-80 `;
+
     return (
-        <nav className="footer__menu">
-            <Link to="/" className="">
+        <nav className="
+        flex
+        flex-wrap
+        justify-center
+        items-center
+        gap-4
+        md:gap-6
+        ">
+            <Link to="/" className={estilosLink}>
                 Início
             </Link>
 
-            <Link to="/sobre" className="">
+            <Link to="/sobre" className={estilosLink}>
                 A SoulMove
             </Link>
 
-            <Link to="/funcionalidades" className="">
+            <Link to="/funcionalidades" className={estilosLink}>
                 Como Funciona
             </Link>
 
-            <Link to="/roadmap" className="">
+            <Link to="/roadmap" className={estilosLink}>
                 Visão de Futuro
             </Link>
 
-            <Link to="/integrantes" className="">
+            <Link to="/integrantes" className={estilosLink}>
                 Nosso Time
             </Link>
 
-            <Link to="/faq" className="">
+            <Link to="/faq" className={estilosLink}>
                 FAQ
             </Link>
 
-            <Link to="/contato" className="">
+            <Link to="/contato" className={estilosLink}>
                 Contato
             </Link>
         </nav>
