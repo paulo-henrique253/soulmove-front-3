b21b7c959621267a271fc69f5b6bcec2490140e4
Felle <juliafb1903@gmail.com>
2026-09-09 19:29:09 -0300

    feat: componentes footer e menu do footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
+import MenuFooter from "./MenuFooter";
 
-function Footer(){
+function Footer() {
     return (
-        <></>
+        <footer className="">
+            
+            <MenuFooter />
+
+            <p className="">
+                © 2026 Antípoda. Todos os direitos reservados.
+            </p>
+        </footer>
     );
 }
 
---
+++ src/components/MenuFooter.tsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router";
+
+function MenuNavegacao() {
+    return (
+        <nav className="footer__menu">
+            <Link to="/" className="">
+                Início
+            </Link>
+
+            <Link to="/sobre" className="">
+                A SoulMove
+            </Link>
+
+            <Link to="/funcionalidades" className="">
+                Como Funciona
+            </Link>
+
+            <Link to="/roadmap" className="">
+                Visão de Futuro
+            </Link>
+
+            <Link to="/integrantes" className="">
+                Nosso Time
+            </Link>
+
+            <Link to="/faq" className="">
+                FAQ
+            </Link>
+
+            <Link to="/contato" className="">
+                Contato
+            </Link>
+        </nav>
+    );
+}
+
+export default MenuNavegacao;
