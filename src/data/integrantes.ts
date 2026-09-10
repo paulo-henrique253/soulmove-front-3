e51d5367287c45b8eaee050f5853b5242c97f708
Felle <juliafb1903@gmail.com>
2026-09-10 19:13:07 -0300

    feat: data integrantes

---
+++ src/data/integrantes.ts
@@ -0,0 +1,63 @@
+export type Integrante = {
+  nome: string;
+  slug: string;
+  turma: string;
+  rm: string;
+  foto: string;
+  github: string;
+  linkedin: string;
+  sobre: string;
+};
+
+export const integrantes: Integrante[] = [
+  {
+    nome: "Enzo Zeni",
+    slug: "enzo",
+    turma: "1TDSPK",
+    rm: "RM570653",
+    foto: "/assets/zeni.png",
+    github: "https://github.com/F-Zeni",
+    linkedin: "https://www.linkedin.com/in/enzo-fachinelli-zeni-687a873b3",
+    sobre: "adicionar",
+  },
+  {
+    nome: "Jacqueline Lotufo",
+    slug: "jacqueline",
+    turma: "1TDSPK",
+    rm: "RM571390",
+    foto: "/assets/jac.png",
+    github: "https://github.com/jacquelot",
+    linkedin: "https://br.linkedin.com/in/jacqueline-lotufo",
+    sobre: "adicionar",
+  },
+  {
+    nome: "Julia de Freitas",
+    slug: "julia",
+    turma: "1TDSPK",
+    rm: "RM569098",
+    foto: "/assets/ju.png",
+    github: "https://github.com/lixzaura",
+    linkedin: "https://www.linkedin.com/in/lixzaura",
+    sobre: "adicionar",
+  },
+  {
+    nome: "Paulo Henrique",
+    slug: "paulo",
+    turma: "1TDSPK",
+    rm: "RM573806",
+    foto: "/assets/ph.png",
+    github: "https://github.com/paulo-henrique253",
+    linkedin: "https://www.linkedin.com/in/paulo-henrique-gomes-gon%C3%A7alves-aa04b5272/",
+    sobre: "adicionar",
+  },
+  {
+    nome: "Danielle Fernanda",
+    slug: "danielle",
+    turma: "1TDSPK",
+    rm: "RM569359",
+    foto: "/assets/dani.png",
+    github: "https://github.com/daniellekgn",
+    linkedin: "https://www.linkedin.com/in/danielle-fernanda-kagan-9026b141b/",
+    sobre: "adicionar",
+  },
+];
