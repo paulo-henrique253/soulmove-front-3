6840c129c508a6e3ebf4d84135d6596a756b0095
Zeni <enzozeni12@gmail.com>
2026-09-11 21:48:09 -0300

    feat: componente cardfuncionalidades criado

---
+++ src/components/CardFuncionalidade.tsx
@@ -0,0 +1,44 @@
+import ImagemInterativa from "./ImagemInterativa";
+
+type CardFuncionalidadeProps = {
+    inverterOrdem: boolean;
+    titulo: string;
+    descricao: string;
+    imagemPrincipalSrc: string;
+    imagemPrincipalAlt: string;
+    imagemSecundarioSrc: string;
+    imagemSecundarioAlt: string;
+};
+
+function CardFuncionalidade( { inverterOrdem = false, titulo, descricao, imagemPrincipalSrc, imagemPrincipalAlt, imagemSecundarioSrc, imagemSecundarioAlt}: CardFuncionalidadeProps ) {
+        
+    
+    return (
+            <section className={`flex flex-col ${inverterOrdem ? "md:flex-row-reverse" : "md:flex-row"} items-center justify-between gap-12 py-12 border-b border-gray-100 last:border-0`}>
+                <div className="flex-1 space-y-5">
+                    <h2 className="text-3xl font-bold text-gray-800">
+                        {titulo}
+                    </h2>
+
+                    <p className="text-lg text-gray-600 leading-relaxed">
+                        {descricao}
+                    </p>
+
+                    <p className="inline-block px-4 py-2 text-sky-400 font-semibold text-sm">
+                        Interaja com a imagem!
+                    </p>
+                </div>
+
+                <div className="flex-1 flex justify-center md:justify-end">
+                    <ImagemInterativa 
+                    principalSrc={imagemPrincipalSrc}
+                    principalAlt={imagemPrincipalAlt}
+                    secundarioSrc={imagemSecundarioSrc}
+                    secundarioAlt={imagemSecundarioAlt}
+                    />
+                </div>
+            </section>
+        );
+}
+
+export default CardFuncionalidade;
