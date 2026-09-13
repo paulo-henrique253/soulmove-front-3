c51e132f1e0d9e3369b09f050646e0bb591d03e6
paulo-henrique253 <paulo253hgg@gmail.com>
2026-09-13 04:05:48 -0300

    Fix: layout faq mobile

--- src/components/FaqCard.tsx
+++ src/components/FaqCard.tsx
@@ -12,7 +12,7 @@ function FaqCard({pergunta, resposta, index}: FaqCardProps) {
 return(
     <div className="w-75 h-min md:h-auto md:w-90 bg-linear-to-t from-indigo-500 to-sky-400 rounded-2xl p-4">
         <button 
-            className="w-full h-20 md:h-auto bg-white rounded-4xl px-5 py-3 font-['Momo_Trust_Display'] text-lg"
+            className="w-full sm:h-20 md:h-auto bg-white rounded-4xl px-5 py-3 font-['Momo_Trust_Display'] text-lg"
             onClick={trocarPergunta}
         >{pergunta}</button>
         <p className="hidden md:block px-3 pt-3 text-center font-['Lexend_Deca'] text-black"
